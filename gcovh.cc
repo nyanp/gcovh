46eb3bbdf52e12ef14d83f1bcf42495641432952
Taiga Noumi <Noumi.Taiga@gmail.com>
2012-07-24 07:22:36 +0900

    refactoring

--- gcovh.cc
+++ gcovh.cc
@@ -1,5 +1,7 @@
 #include "gcovh.h"
 #include <iostream>
+#include <vector>
+#include <map>
 
 void howto(void) {
 	std::cout << "gcovr - gcov html report generator" << std::endl;
@@ -7,15 +9,22 @@ void howto(void) {
 }
 
 
+typedef std::map<std::string, gcovh::parsed_source> sources_t;
+
 int main (int argc, char *argv[]) {
 	if (argc < 2) {
 		std::cout << "error: invalid arg" << std::endl;
 		return -1;
 	}
 
+	sources_t sources;
+
 	for (int i = 1; i < argc; i++) {
-		gcovh::parsed_source content = gcovh::parse(argv[i]);
-		gcovh::write(content, std::string(argv[i]) + ".html");
+		sources[argv[i]] = gcovh::parse(argv[i]);
+	}
+
+	for (sources_t::iterator it = sources.begin(), end = sources.end(); it != end; ++it) {
+		gcovh::write((*it).second, (*it).first + ".html");
 	}
 
 }
